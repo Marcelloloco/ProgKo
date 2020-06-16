c69fe0fc25c130dd850bf0553232d6e9cdbc8fe7
Marcel Trattner <m.trattner99@gmail.com>
2020-06-16 12:34:03 +0200

    Added functionality for embossing

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,17 @@ using namespace cv;
 //function declaration
 Mat applyGreyscale(Mat image);
 Mat convertBGR2HSV(Mat image);
+Mat applyEmbossFilter(Mat image);
+
+const int filter_width = 3;
+const int filter_height = 3;
+
+int filter[filter_width][filter_height] =
+{
+    -1, -1, 0,
+    -1, 0, 1,
+    0, 1, 1
+};
 
 int main(int argc, char const *argv[])
 {
@@ -38,14 +49,17 @@ int main(int argc, char const *argv[])
             printf("No image data \n");
             return -1;
         }
-        Mat gray_image = applyGreyscale(image);
-        imwrite( "../out/greyscale/" + inputs[i], gray_image );
-        //std::cout << inputs[i] << std::endl;
-        imwrite( "../out/hsv/" + inputs[i], convertBGR2HSV(image) );
-        namedWindow(inputs[i], WINDOW_AUTOSIZE );
-        imshow(inputs[i], convertBGR2HSV(image));
 
-        waitKey(0);
+        std::cout << "File: " << inputs[i] << std::endl;
+        imwrite( "../out/greyscale/" + inputs[i], applyGreyscale(image));
+        imwrite( "../out/hsv/" + inputs[i], convertBGR2HSV(image));
+        imwrite( "../out/emboss/" + inputs[i], applyEmbossFilter(image));
+        std::cout << "saved" << std::endl;
+
+        //Following lines are for displaying images
+        //namedWindow(inputs[i], WINDOW_AUTOSIZE );
+        //imshow(inputs[i], img);
+        //waitKey(0);
     }
 
     return 0;
@@ -56,9 +70,12 @@ Mat applyGreyscale(Mat image)
     //initializing a zero Mat array from the input image
     Mat new_image = Mat::zeros( image.size(), image.type() );
 
-    for( int y = 0; y < image.rows; y++ ) {
-        for( int x = 0; x < image.cols; x++ ) {
-            for( int c = 0; c < image.channels(); c++ ) {
+    for(int y = 0; y < image.rows; y++) 
+    {
+        for(int x = 0; x < image.cols; x++) 
+        {
+            for(int c = 0; c < image.channels(); c++) 
+            {
                 Vec3b intensity = image.at<Vec3b>(y, x);
                 new_image.at<Vec3b>(y,x)[c] = 0.07 * intensity.val[0] + 0.72 * intensity.val[1] + 0.21 * intensity.val[2];
             }
@@ -73,8 +90,10 @@ Mat convertBGR2HSV(Mat image)
     //initializing a zero Mat array from the input image
     Mat new_image = Mat::zeros( image.size(), image.type());
 
-    for( int y = 0; y < image.rows; y++ ) {
-        for( int x = 0; x < image.cols; x++ ) {
+    for(int y = 0; y < image.rows; y++) 
+    {
+        for(int x = 0; x < image.cols; x++) 
+        {
             Vec3b intensity = image.at<Vec3b>(y, x);
             int h;
             int s;
@@ -124,5 +143,59 @@ Mat convertBGR2HSV(Mat image)
         }
     }
 
+    return new_image;
+}
+
+Mat applyEmbossFilter(Mat image)
+{
+    //initializing a zero Mat array from the input image
+    Mat new_image = Mat::zeros( image.size(), image.type() );
+
+    for(int y = 0; y < image.rows; y++) 
+    {
+        for(int x = 0; x < image.cols; x++) 
+        {
+            Vec3b intensity = image.at<Vec3b>(y, x);
+
+            double red = 0.0, green = 0.0, blue = 0.0;
+
+            for (size_t i = 0; i < filter_width; i++)
+            {
+                for (size_t j = 0; j < filter_height; j++)
+                {
+                    int yPos;
+                    yPos = y - (i - 1);
+                    if (yPos < 0)
+                    {
+                        yPos = 0;
+                    }
+                    else if (yPos > image.rows)
+                    {
+                        yPos = image.rows;
+                    }
+                    
+                    int xPos;
+                    xPos = x - (j - 1);
+                    if (xPos < 0)
+                    {
+                        xPos = 0;
+                    }
+                    else if (xPos > image.cols)
+                    {
+                        xPos = image.cols;
+                    }
+                    
+                    red += image.at<Vec3b>(yPos,xPos)[2] * filter[i][j];
+                    green += image.at<Vec3b>(yPos,xPos)[1] * filter[i][j];
+                    blue += image.at<Vec3b>(yPos,xPos)[0] * filter[i][j];
+                }
+                
+            }
+
+            new_image.at<Vec3b>(y,x)[2] = red;
+            new_image.at<Vec3b>(y,x)[1] = green;
+            new_image.at<Vec3b>(y,x)[0] = blue;
+        }
+    }
     return new_image;
 }
